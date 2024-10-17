8405b4b9abb6cf693ff6c628d496a052999f7c9f
Madhan Kumar <madhankumarbusiness@gmail.com>
2024-10-17 23:24:49 +0530

    contributors change

--- components/main/Contributors.tsx
+++ components/main/Contributors.tsx
@@ -1,56 +1,71 @@
 "use client";
-import { CardStack } from "../ui/card-stack";
-import { cn } from "@/utils/cn";
+// import { useEffect } from "react";
+import { AnimatedTooltip } from "../ui/animated-tooltip";
+// import { CardStack } from "../ui/card-stack";
+// import { cn } from "@/utils/cn";
+import axios, { AxiosResponse } from "axios";
+import { useEffect, useState } from "react";
 
 export function Contributors() {
+
+  const [contributors, setContributors] = useState<AxiosResponse[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  async function updateContributors() {
+    try {
+      const response = await axios.get(
+        "https://api.github.com/repos/Harish-M-2003/Squig-next/contributors"
+      );
+      setContributors(response.data);
+
+    } catch (error) {
+      console.error("Failed to fetch contributors:", error);
+      setContributors([]);
+    }
+
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    updateContributors();
+  }, []);
+
   return (
-    <div className="h-[40rem] flex flex-col items-center justify-center w-full bg-gradient-to-b from-gray-300 via-gray-300 dark:from-gray-900 dark:via-gray-800">
+    <div className="flex flex-col items-center justify-center w-full h-[20em]">
       <div className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 mb-8">
         Contributors
       </div>
-      <CardStack items={CARDS} />
+
+      {
+        (loading) ?
+          <></>
+          :
+          <div className="flex">
+            <AnimatedTooltip items={contributors} />
+          </div>
+      }
+
     </div>
   );
 }
 
-export const Highlight = ({
-  children,
-  className,
-}: {
-  children: React.ReactNode;
-  className?: string;
-}) => {
-  return (
-    <span
-      className={cn(
-        "font-bold bg-emerald-100 text-emerald-700 dark:bg-emerald-700/[0.2] dark:text-emerald-500 px-1 py-0.5 mt-0",
-        className
-      )}
-    >
-      {children}
-    </span>
-  );
-};
+// export const Highlight = ({
+//   children,
+//   className,
+// }: {
+//   children: React.ReactNode;
+//   className?: string;
+// }) => {
+//   return (
+//     <span
+//       className={cn(
+//         "font-bold bg-emerald-100 text-emerald-700 dark:bg-emerald-700/[0.2] dark:text-emerald-500 px-1 py-0.5 mt-0",
+//         className
+//       )}
+//     >
+//       {children}
+//     </span>
+//   );
+// };
 
-const CARDS = [
-  {
-    id: 0,
-    name: "Harish",
-    designation: "Founder",
-    content: (
-      <p>
-        As the founder, <Highlight>Harish</Highlight> has led the way with vision and dedication. Currently in the 4th year of studies, he has played a pivotal role in shaping the project.
-      </p>
-    ),
-  },
-  {
-    id: 1,
-    name: "Madhan Kumar M",
-    designation: "Contributor",
-    content: (
-      <p>
-        <Highlight>Madhan Kumar M</Highlight> is a contributor currently studying in the 3rd year. His contributions have been instrumental in the development and success of our project.
-      </p>
-    ),
-  },
-];
+
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["assets.aceternity.com", "api.microlink.io"],
+    domains: ["assets.aceternity.com", "api.microlink.io",'avatars.githubusercontent.com'],
   },
 };
 
